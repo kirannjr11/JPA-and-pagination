ddf6034ef84f7f89583e1d00d49ea0f758775ac9
kiran <ghalekiran03@gmail.com>
2024-08-02 22:35:43 +0900

    test cases

--- src/main/java/com/example/hibernate/repository/ProductRepository.java
+++ src/main/java/com/example/hibernate/repository/ProductRepository.java
@@ -4,6 +4,16 @@ import com.example.hibernate.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    List<ProductEntity> findByTitle(String title);
+    List<ProductEntity> findByCreatedAtAfter(LocalDateTime after);
+//    List<ProductEntity> findQuantityGreaterThanOrPriceLessThan(int quantity, BigDecimal price);
+    List<ProductEntity> findByTitleLike(String title);
+
 }
--- src/test/java/com/example/hibernate/HibernateApplicationTests.java
+++ src/test/java/com/example/hibernate/HibernateApplicationTests.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class HibernateApplicationTests {
@@ -17,15 +20,60 @@ class HibernateApplicationTests {
 	void contextLoads() {
 	}
 
+//	@Test
+//	void testRepository() {
+//		ProductEntity productEntity = ProductEntity.builder()
+//				.sku("waiwaui")
+//				.title("chocolate")
+//				.price(BigDecimal.valueOf(123.45))
+//				.quantity(12)
+//				.build();
+//		ProductEntity savedProductEntity = productRepository.save(productEntity);
+//		System.out.println(savedProductEntity);
+//	}
+
 	@Test
-	void testRepository() {
-		ProductEntity productEntity = ProductEntity.builder()
-				.sku("nestley")
-				.title("chocolate")
-				.price(BigDecimal.valueOf(123.45))
-				.quantity(12)
-				.build();
-		ProductEntity savedProductEntity = productRepository.save(productEntity);
-		System.out.println(savedProductEntity);
+	void getRepository() {
+		List<ProductEntity> entities = productRepository.findAll();
+		System.out.println(entities);
+	}
+
+	@Test
+	void getByTitile() {
+		List<ProductEntity> entities = productRepository.findByTitle("chocolate");
+		System.out.println(entities);
+	}
+
+	@Test
+	void getById() {
+		Optional<ProductEntity> optionalEntity = productRepository.findById(28L);
+		if (optionalEntity.isPresent()) {
+			ProductEntity entity = optionalEntity.get();
+			System.out.println(entity);
+		} else {
+			System.out.println("Product not found");
+		}
+	}
+
+	@Test
+	void getByAfter() {
+		LocalDateTime date = LocalDateTime.of(2024, 7, 31, 0, 0);
+		List<ProductEntity> entities = productRepository.findByCreatedAtAfter(date);
+//		if (!entities.isEmpty()) {
+//			entities.forEach(System.out::println);
+//		} else {
+//			System.out.println("Products not found");
+//		}
+		if(!entities.isEmpty()) {
+			System.out.println(entities);
+		} else {
+			System.out.println("resource not found");
+		}
+	}
+
+	@Test
+	void getTitleLike() {
+		List<ProductEntity> entities = productRepository.findByTitleLike("%Ch%");
+		System.out.println(entities);
 	}
 }
